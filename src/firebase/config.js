0ff14b4799082b1aad122b9444d850a2a91bec0d
DS_Developer <124428454+ChaoticBrain-Dead@users.noreply.github.com>
2024-03-16 20:17:59 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,14 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAeM7c3pDRpNNClEm8eRwHYShvucIgyuMM",
+  apiKey: "AIzaczfvdzcdzczdccuMM",
   authDomain: "fintick.firebaseapp.com",
   databaseURL: "https://fintick-default-rtdb.firebaseio.com",
   projectId: "fintick",
   storageBucket: "fintick.appspot.com",
-  messagingSenderId: "484080883987",
-  appId: "1:484080883987:web:a54d5cb83fb78b887d62e6",
-  measurementId: "G-LJZJ6KD54D"
+  messagingSenderId: "344534583987",
+  appId: "1:48408088cxzcxzcv
+  measurementId: "G-LJxzcxzcvxD"
 };
 
 // Initialize Firebase
